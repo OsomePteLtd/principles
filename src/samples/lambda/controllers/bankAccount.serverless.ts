45dca5e444f622005517a9d36f04dacc503ce6e1
Alexander Balashov <divineforest@osome.com>
2021-09-02 18:18:09 +0200

    backend: lambda controller sample with sls config and handlers file (#54)

---
+++ src/samples/lambda/controllers/bankAccount.serverless.ts
@@ -0,0 +1,69 @@
+import type { Functions } from 'serverless/aws';
+
+export const bankAccount: Functions = {
+  'create-bank-account': {
+    handler: 'src/controllers/bankAccount/handlers.create',
+    events: [
+      {
+        http: {
+          path: 'api/v2/accounting/companies/{companyId}/bank_accounts',
+          method: 'post',
+          authorizer: '${self:custom.authorizer}',
+          cors: true,
+        },
+      },
+    ],
+  },
+  'get-bank-accounts': {
+    handler: 'src/controllers/bankAccount/handlers.index',
+    events: [
+      {
+        http: {
+          path: 'api/v2/accounting/companies/{companyId}/bank_accounts',
+          method: 'get',
+          authorizer: '${self:custom.authorizer}',
+          cors: true,
+        },
+      },
+    ],
+  },
+  'get-bank-account': {
+    handler: 'src/controllers/bankAccount/handlers.show',
+    events: [
+      {
+        http: {
+          path: 'api/v2/accounting/bank_accounts/{bankAccountId}',
+          method: 'get',
+          authorizer: '${self:custom.authorizer}',
+          cors: true,
+        },
+      },
+    ],
+  },
+  'update-bank-account': {
+    handler: 'src/controllers/bankAccount/handlers.update',
+    events: [
+      {
+        http: {
+          path: 'api/v2/accounting/bank_accounts/{bankAccountId}',
+          method: 'patch',
+          authorizer: '${self:custom.authorizer}',
+          cors: true,
+        },
+      },
+    ],
+  },
+  'delete-bank-account': {
+    handler: 'src/controllers/bankAccount/handlers.destroy',
+    events: [
+      {
+        http: {
+          path: 'api/v2/accounting/bank_accounts/{bankAccountId}',
+          method: 'delete',
+          authorizer: '${self:custom.authorizer}',
+          cors: true,
+        },
+      },
+    ],
+  },
+};
---
+++ src/samples/lambda/controllers/handlers.ts
@@ -0,0 +1,1 @@
+export * from './bankAccount.controller';
