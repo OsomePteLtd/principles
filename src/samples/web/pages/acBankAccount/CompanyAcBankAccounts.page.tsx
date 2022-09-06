6ba8d0816d8784efa6c05a3d1d05f65410df3952
Artem Lipatov <artem.lipatov@osome.com>
2022-09-06 22:47:28 +0300

    add web frontend sample

---
+++ src/samples/web/pages/acBankAccount/CompanyAcBankAccounts.page.tsx
@@ -0,0 +1,43 @@
+import { PageLayout, PageTitle, PageSkeleton } from '@osome/ui-kit';
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+
+import { CompanyAcBankAccounts } from '../../components/acBankAccount/CompanyAcBankAccounts/CompanyAcBankAccounts';
+import { useGetAcCompanyBankAccounts } from '../../queries/acBankAccount.query';
+import { useGetCompany } from '../../queries/company.service';
+import { useGetMe } from '../../queries/user.service';
+import { hasUserAccessToCompany } from '../../services/company.service';
+
+type CompanyAcBankAccountsPageProps = {
+  companyId: number;
+};
+
+export const CompanyAcBankAccountsPage = (props: CompanyAcBankAccountsPageProps) => {
+  const { companyId } = props;
+
+  const companyQuery = useGetCompany(companyId);
+  const company = companyQuery.data?.company;
+
+  const userQuery = useGetMe(companyId);
+  const user = userQuery.data?.user;
+
+  // prefetch bank accounts
+  useGetAcCompanyBankAccounts(companyId);
+
+  if (!company || !user) {
+    return <PageSkeleton />;
+  }
+
+  const hasAccess = hasUserAccessToCompany(user, company);
+
+  if (!hasAccess) {
+    return <Redirect to="/companies" />;
+  }
+
+  return (
+    <PageLayout>
+      <PageTitle>Bank Accounts</PageTitle>
+      <CompanyAcBankAccounts companyId={companyId} />
+    </PageLayout>
+  );
+};
---
+++ src/samples/web/queries/acBankAccount.query.ts
@@ -0,0 +1,81 @@
+import { AcBankAccountNew, AcBankAccountResponse, AcBankAccountUpdate } from '@osome/sdk';
+import { AxiosError } from 'axios';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
+
+import { sdk } from '../services/sdk.service';
+
+const acBankAccountCacheKey = (bankAccountId: number) => ['acBankAccount', bankAccountId];
+const companyAcBankAccountsCacheKey = (companyId: number) => ['acCompanyBankAccounts', companyId];
+
+export function useCreateAcBankAccount() {
+  const queryClient = useQueryClient();
+
+  return useMutation<AcBankAccountResponse, AxiosError, AcBankAccountNew>(
+    (bankAccountNew) =>
+      sdk.accounting.companies
+        .companyId(bankAccountNew.companyId)
+        .bank_accounts.post({ bankAccount: bankAccountNew }),
+    {
+      onSuccess(responseBody) {
+        const { id, companyId } = responseBody.bankAccount;
+        queryClient.setQueryData(acBankAccountCacheKey(id), responseBody);
+        queryClient.invalidateQueries(companyAcBankAccountsCacheKey(companyId));
+      },
+    },
+  );
+}
+
+export function useGetAcBankAccount(bankAccountId: number) {
+  return useQuery(acBankAccountCacheKey(bankAccountId), () =>
+    sdk.accounting.bank_accounts.id(bankAccountId).get(),
+  );
+}
+
+export function usePatchAcBankAccount(bankAccountId: number) {
+  const queryClient = useQueryClient();
+
+  return useMutation<AcBankAccountResponse, AxiosError, AcBankAccountUpdate>(
+    (bankAccountUpdate) =>
+      sdk.accounting.bank_accounts.id(bankAccountId).patch({ bankAccount: bankAccountUpdate }),
+    {
+      onSuccess(responseBody) {
+        const { id, companyId } = responseBody.bankAccount;
+        queryClient.setQueryData(acBankAccountCacheKey(id), responseBody);
+        queryClient.invalidateQueries(companyAcBankAccountsCacheKey(companyId));
+      },
+    },
+  );
+}
+
+export function useDeleteAcBankAccount(bankAccountId: number) {
+  const queryClient = useQueryClient();
+
+  return useMutation<AcBankAccountResponse, AxiosError, AcBankAccountUpdate>(
+    (requestBody) => sdk.accounting.bank_accounts.id(bankAccountId).delete(requestBody),
+    {
+      onSuccess(responseBody) {
+        const { id, companyId } = responseBody.bankAccount;
+        queryClient.removeQueries(acBankAccountCacheKey(id));
+        queryClient.invalidateQueries(companyAcBankAccountsCacheKey(companyId));
+      },
+    },
+  );
+}
+
+export function useGetAcCompanyBankAccounts(companyId: number) {
+  const queryClient = useQueryClient();
+
+  return useQuery(
+    companyAcBankAccountsCacheKey(companyId),
+    () => sdk.accounting.companies.companyId(companyId).bank_accounts.get(),
+    {
+      onSuccess(responseBody) {
+        responseBody.bankAccounts.forEach((bankAccount) =>
+          queryClient.setQueryData(acBankAccountCacheKey(bankAccount.id), {
+            bankAccount,
+          }),
+        );
+      },
+    },
+  );
+}
