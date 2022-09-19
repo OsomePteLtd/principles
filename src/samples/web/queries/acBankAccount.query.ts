e916a5ad88679789a1d6aa5887497527be881fbe
Artem <Rendalf@users.noreply.github.com>
2022-09-19 15:13:05 +0300

    PL-1201 Add web frontend sample (#199)
    * add web frontend sample
    
    * prettier
    
    * type -> interface
    
    * fix wrong nesting
    
    * add perm check
    
    * remove companyId from useGetMe
    
    * preloader
    
    * update
    
    * get prefix for cache key
    
    * destructure query

---
+++ src/samples/web/queries/acBankAccount.query.ts
@@ -0,0 +1,80 @@
+import { AcBankAccountNew, AcBankAccountResponse, AcBankAccountUpdate } from '@osome/sdk';
+import { useMutation, useQuery, useQueryClient, QueryClient } from 'react-query';
+
+import { sdk } from '../services/sdk.service';
+
+const getAcBankAccountCacheKey = (bankAccountId: number) => ['acBankAccount', bankAccountId];
+const companyAcBankAccountsCacheKey = (companyId: number) => ['acCompanyBankAccounts', companyId];
+
+export function useCreateAcBankAccount() {
+  const queryClient = useQueryClient();
+
+  return useMutation(
+    (bankAccountNew: AcBankAccountNew) =>
+      sdk.accounting.companies
+        .companyId(bankAccountNew.companyId)
+        .bank_accounts.post({ bankAccount: bankAccountNew }),
+    {
+      onSuccess(responseBody) {
+        const { id, companyId } = responseBody.bankAccount;
+        queryClient.setQueryData(getAcBankAccountCacheKey(id), responseBody);
+        queryClient.invalidateQueries(companyAcBankAccountsCacheKey(companyId));
+      },
+    },
+  );
+}
+
+export function useGetAcBankAccount(bankAccountId: number) {
+  return useQuery(getAcBankAccountCacheKey(bankAccountId), () =>
+    sdk.accounting.bank_accounts.id(bankAccountId).get(),
+  );
+}
+
+export function usePatchAcBankAccount(bankAccountId: number) {
+  const queryClient = useQueryClient();
+
+  return useMutation(
+    (bankAccountUpdate: AcBankAccountUpdate) =>
+      sdk.accounting.bank_accounts.id(bankAccountId).patch({ bankAccount: bankAccountUpdate }),
+    {
+      onSuccess(responseBody) {
+        const { companyId } = responseBody.bankAccount;
+        updateAcBankAccountCache(queryClient, responseBody);
+        updateAcBankAccountCache(queryClient, responseBody);
+        queryClient.invalidateQueries(companyAcBankAccountsCacheKey(companyId));
+      },
+    },
+  );
+}
+
+export function useDeleteAcBankAccount(bankAccountId: number) {
+  const queryClient = useQueryClient();
+
+  return useMutation(() => sdk.accounting.bank_accounts.id(bankAccountId).delete(), {
+    onSuccess(responseBody) {
+      const { id, companyId } = responseBody.bankAccount;
+      queryClient.removeQueries(getAcBankAccountCacheKey(id));
+      queryClient.invalidateQueries(companyAcBankAccountsCacheKey(companyId));
+    },
+  });
+}
+
+export function useGetAcCompanyBankAccounts(companyId: number) {
+  const queryClient = useQueryClient();
+
+  return useQuery(
+    companyAcBankAccountsCacheKey(companyId),
+    () => sdk.accounting.companies.companyId(companyId).bank_accounts.get(),
+    {
+      onSuccess(responseBody) {
+        responseBody.bankAccounts.forEach((bankAccount) =>
+          updateAcBankAccountCache(queryClient, { bankAccount }),
+        );
+      },
+    },
+  );
+}
+
+function updateAcBankAccountCache(queryClient: QueryClient, responseBody: AcBankAccountResponse) {
+  queryClient.setQueryData(getAcBankAccountCacheKey(responseBody.bankAccount.id), responseBody);
+}
